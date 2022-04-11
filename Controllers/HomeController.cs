73b006ab5fdfbccc472dc4746e5014aefe7e0e0f
Martin Thwaites <github@my2cents.co.uk>
2022-04-12 00:00:26 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace o11y_intro.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public HomeController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("/calculate")]
+    public async Task<int> Calculate(int index = 0)
+    {
+        // CUSTOM ATTRIBUTES
+        Activity.Current?.SetTag("parameter.index", index);
+
+        if (index == 0 | index == 1)
+            return 0;
+        if (index == 2)
+            return 1;
+
+        var resOne = await GetNext(index - 1);
+        var resTwo = await GetNext(index - 2);
+
+        // CHILD SPAN (2 sections of code to uncomment, 2 lines total)
+        using var span = Activity.Current?.Source.StartActivity("calculation");
+
+        var fibonacciNumber = resOne + resTwo;
+        span?.SetTag("result", fibonacciNumber);
+        
+        return fibonacciNumber;
+    }
+
+    private async Task<int> GetNext(int iv)
+    {
+        var resp = await _httpClient.GetAsync($"http://{HttpContext.Request.Host}/calculate?index={iv}");
+        resp.EnsureSuccessStatusCode();
+        return int.Parse(await resp.Content.ReadAsStringAsync());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Honeycomb.OpenTelemetry;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHttpClient()
+                .AddControllersWithViews();
+
+// Open Telemetry Setup
+
+builder.Services.AddHoneycomb(builder.Configuration);
+
+// End Open Telemetry Setup
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
